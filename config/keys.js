855f7502bfed7927396f87a27058343da5ce0a53
“Dongzhe <wdz19910402@gmail.com>
2020-06-14 19:57:16 -0700

    add keys

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '535748134874-v9ri7io2qjmdnsgkesnu171ra6mv7hf6.apps.googleusercontent.com',
+    googleClientSecret: 'SiNsJkNpsFtxMJWGIKKAfZfl',
+    mongoURI: 'mongodb+srv://zhezhe:YPg4dNiWehW6xeSk@emaily-vlxlt.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    cookieKey: '123456789'
+}
