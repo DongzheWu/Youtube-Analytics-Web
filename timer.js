58854e1d623cfeb1351dfda2b89b8810b564ee88
“Dongzhe <wdz19910402@gmail.com>
2020-07-02 20:57:44 -0700

    changed timer

--- timer.js
+++ timer.js
@@ -1,57 +1,65 @@
 
+
+var count = 1;
+function getAPIKey(){
+    count++;
+    count = count % 6;
+    return keys.googleAPIKeys[count];
+}
+
 function timer(){
-    console.log("hello");
+
+    const express = require('express');
+    const mongoose = require('mongoose');
+    const {google} = require('googleapis');
+    const keys = require('./config/keys');
+
+    const Video = mongoose.model('videos');
+    console.log("run");
+    console.log("runrurnrurnrunrurnrurn");
+    Video.find(function(err, videos){
+        videos.forEach(async(video) => {
+
+            if(video.recentViewCount){
+                await google.youtube('v3').videos.list({
+                    key: getAPIKey(),
+                    part: 'statistics',
+                    id: video.videoId,
+                }).then(async(rep) => {
+                    var dt = new Date();
+                    await Video.updateOne({
+                        googleId: video.googleId,
+                        keyword: video.keyword,
+                        videoId: video.videoId
+                   
+                    },
+                    {
+                        $push:{
+                            values: rep.data.items[0].statistics.viewCount - video.recentViewCount,
+                            redates: dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate()
+                        },
+                        recentViewCount: rep.data.items[0].statistics.viewCount
+    
+                    });
+                });
+            }else{
+                await google.youtube('v3').videos.list({
+                    key: getAPIKey(),
+                    part: 'statistics',
+                    id: video.videoId,
+                }).then(async(rep) => {
+                   video.recentViewCount =  rep.data.items[0].statistics.viewCount;
+                   await video.save();
+                });
+            }
+
+
+        });
+    }); 
 }
 
 
-setInterval(timer, 1000 * 60);
+setInterval(timer, 1000 * 60 * 5);
     
    
-    // require('../models/Video');
-    // const mongoose = require('mongoose');
-    // const {google} = require('googleapis');
-    // const keys = require('../config/keys');
 
-    // const Video = mongoose.model('videos');
-    // console.log("run");
-    // console.log("runrurnrurnrunrurnrurn");
-    // Video.find(function(err, videos){
-    //     videos.forEach(async(video) => {
-
-    //         if(video.recentViewCount){
-    //             await google.youtube('v3').videos.list({
-    //                 key: getAPIKey(),
-    //                 part: 'statistics',
-    //                 id: video.videoId,
-    //             }).then(async(rep) => {
-    //                 var dt = new Date();
-    //                 await Video.updateOne({
-    //                     googleId: video.googleId,
-    //                     keyword: video.keyword,
-    //                     videoId: video.videoId
-                   
-    //                 },
-    //                 {
-    //                     $push:{
-    //                         values: rep.data.items[0].statistics.viewCount - video.recentViewCount,
-    //                         redates: dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate()
-    //                     },
-    //                     recentViewCount: rep.data.items[0].statistics.viewCount
-    
-    //                 });
-    //             });
-    //         }else{
-    //             await google.youtube('v3').videos.list({
-    //                 key: getAPIKey(),
-    //                 part: 'statistics',
-    //                 id: video.videoId,
-    //             }).then(async(rep) => {
-    //                video.recentViewCount =  rep.data.items[0].statistics.viewCount;
-    //                await video.save();
-    //             });
-    //         }
-
-
-
-    //     });
-    // }); 
