6dffabdedbd786d857fefea2639bfa7083f70b60
“Dongzhe <wdz19910402@gmail.com>
2020-07-02 23:03:37 -0700

    add timer

--- index.js
+++ index.js
@@ -478,7 +478,75 @@ app.post('/track/new', async (req, res) =>{
 // }
   
 
+const Video = mongoose.model('videos');
 
+
+
+   
+
+module.exports.timer = function(){
+    
+    var count = 1;
+    function getAPIKey(){
+        count++;
+        count = count % 6;
+        return keys.googleAPIKeys[count];
+    }
+
+    function getNewCount(){
+
+        
+
+        console.log("run");
+        console.log("runrurnrurnrunrurnrurn");
+        Video.find(function(err, videos){
+            console.log("aaaaaaaaaa");
+            console.log(err);
+            console.log(videos);
+            videos.forEach(async(video) => {
+                console.log(video);
+                if(video.recentViewCount){
+                    await google.youtube('v3').videos.list({
+                        key: getAPIKey(),
+                        part: 'statistics',
+                        id: video.videoId,
+                    }).then(async(rep) => {
+                        var dt = new Date();
+                        await Video.updateOne({
+                            googleId: video.googleId,
+                            keyword: video.keyword,
+                            videoId: video.videoId
+                       
+                        },
+                        {
+                            $push:{
+                                values: rep.data.items[0].statistics.viewCount - video.recentViewCount,
+                                redates: dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate()
+                            },
+                            recentViewCount: rep.data.items[0].statistics.viewCount
+        
+                        });
+                    });
+                }else{
+                    console.log("add recent view");
+                    await google.youtube('v3').videos.list({
+                        key: getAPIKey(),
+                        part: 'statistics',
+                        id: video.videoId,
+                    }).then(async(rep) => {
+                       video.recentViewCount =  rep.data.items[0].statistics.viewCount;
+                       await video.save();
+                    });
+                }
+    
+    
+            });
+        }); 
+        
+    }
+    console.log("start");
+    getNewCount();
+}
 
 
 
@@ -488,6 +556,8 @@ require('./routes/authRoutes')(app);
 require('./routes/searchRoute')(app);
 require('./routes/recordRoutes')(app);
 require('./routes/topRoute')(app);
+require('./routes/timer')(app);
+
 // app.get('/', (req, res) => {
 //     res.send({ hi: 'there'});
 //     const User = mongoose.model('users');
---
+++ routes/timer.js
@@ -0,0 +1,68 @@
+module.exports = app => {
+    const {google} = require('googleapis');
+    const keys = require('../config/keys');
+    const Video = mongoose.model('videos');
+
+    var count = 1;
+    function getAPIKey(){
+        count++;
+        count = count % 6;
+        return keys.googleAPIKeys[count];
+    }
+
+    function getNewCount(){
+
+        
+
+        console.log("run");
+        console.log("runrurnrurnrunrurnrurn");
+        Video.find(function(err, videos){
+            console.log("aaaaaaaaaa");
+            console.log(err);
+            console.log(videos);
+            videos.forEach(async(video) => {
+                console.log(video);
+                if(video.recentViewCount){
+                    await google.youtube('v3').videos.list({
+                        key: getAPIKey(),
+                        part: 'statistics',
+                        id: video.videoId,
+                    }).then(async(rep) => {
+                        var dt = new Date();
+                        await Video.updateOne({
+                            googleId: video.googleId,
+                            keyword: video.keyword,
+                            videoId: video.videoId
+                       
+                        },
+                        {
+                            $push:{
+                                values: rep.data.items[0].statistics.viewCount - video.recentViewCount,
+                                redates: dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate()
+                            },
+                            recentViewCount: rep.data.items[0].statistics.viewCount
+        
+                        });
+                    });
+                }else{
+                    console.log("add recent view");
+                    await google.youtube('v3').videos.list({
+                        key: getAPIKey(),
+                        part: 'statistics',
+                        id: video.videoId,
+                    }).then(async(rep) => {
+                       video.recentViewCount =  rep.data.items[0].statistics.viewCount;
+                       await video.save();
+                    });
+                }
+    
+    
+            });
+        }); 
+        app.get('/getNewCount', (req, res) => {
+            getNewCount();
+        });
+        
+        
+    }
+}
